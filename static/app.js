c550f88fd23c6d1304fbaa2d38e82c32d6a17faf
boseriddha <boseriddha@gmail.com>
2023-07-01 18:09:28 +0530

    refactored js file

--- static/app.js
+++ static/app.js
@@ -1,6 +1,11 @@
 const form = document.querySelector("#form");
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  getColors();
+});
+
+function getColors() {
   const query = form.elements.query.value;
   fetch("/palette", {
     method: "POST",
@@ -15,24 +20,29 @@ form.addEventListener("submit", (e) => {
       const colors = data.colors;
       const container = document.querySelector(".container");
       container.replaceChildren([]);
-      for (const color of colors) {
-        const div = document.createElement("div");
-        div.classList.add("color");
-        div.style.backgroundColor = color;
-        const width = 100 / colors.length;
-        div.style.width = `${width}%`;
-
-        div.addEventListener("click", () => {
-          navigator.clipboard.writeText(color);
-        });
-
-        const span = document.createElement("span");
-        span.innerText = color;
-
-        div.appendChild(span);
-
-        container.appendChild(div);
-      }
+      createColors(colors, container);
     });
   });
-});
+}
+
+function createColors(colors, parent) {
+  parent.innerHTML = "";
+  for (const color of colors) {
+    const div = document.createElement("div");
+    div.classList.add("color");
+    div.style.backgroundColor = color;
+    const width = 100 / colors.length;
+    div.style.width = `${width}%`;
+
+    div.addEventListener("click", () => {
+      navigator.clipboard.writeText(color);
+    });
+
+    const span = document.createElement("span");
+    span.innerText = color;
+
+    div.appendChild(span);
+
+    parent.appendChild(div);
+  }
+}
